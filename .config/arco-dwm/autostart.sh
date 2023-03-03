f9009379623603f32b48cb6be185ddb8960f1387
Sammi Turner <ms.sammi.turner@gmail.com>
2023-03-03 12:35:53 +0000

    Add files via upload

---
+++ .config/arco-dwm/autostart.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+function run {
+ if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+run "dex $HOME/.config/autostart/arcolinux-welcome-app.desktop"
+run "pamac-tray"
+run "nitrogen --restore &"
+run "xfce4-power-manager"
+run "blueberry-tray"
+run "/usr/lib/xfce4/notifyd/xfce4-notifyd"
+run "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1"
+picom -b  --config ~/.config/arco-dwm/picom.conf &
+run "numlockx on"
+run "volumeicon"
+run slstatus &
+sxhkd -c ~/.config/arco-dwm/sxhkd/sxhkdrc &
